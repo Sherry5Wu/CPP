4736a015bfad9850e825048b24008a6b759b3ff4
Jingjing Wu <jingwu@c1r5p9.hive.fi>
2025-03-27 07:59:40 +0200

    cpp09 start

---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jingwu <jingwu@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 08:46:32 by jingwu            #+#    #+#             */
+/*   Updated: 2025/03/17 08:46:32 by jingwu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+#include <list>
+
+
+#define RED "\033[0;31m"
+#define GREEN "\033[1;32m"
+#define PURPLE "\033[2;35m"
+#define RESET "\033[0;0m"
+
+void	printInColor(const std::string&  msg, std::string color){
+	std::cout << color << msg << RESET << std::endl;
+}
+
+int main() {
+
+
+    return 0;
+}
